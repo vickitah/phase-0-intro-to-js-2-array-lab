bb16af573cf7bd68968da6e7c90f3d9a4c3e210a
victor <victor.mutuma@student.moringaschool.com>
2025-03-11 15:37:30 +0300

    save completed changes

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+let cats =["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name) {
+    return cats.concat(name); 
+}
+function prependCat(name) {
+    return [name].concat(cats); 
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    return cats.slice(1); 
+}
+
+
+
+
